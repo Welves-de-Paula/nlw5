1fe04e23bd50f9bbf1e73d74568f52c711344979
welves <welvesdepaula@gmail.com>
2021-04-20 20:52:01 -0300

    aula 2

---
+++ src/database/migrations/1618962513299-createSettings.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createSettings1618962513299 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
